348a08f6ce75a519393e4ed260fed2a4a56ce5fa
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-06-14 14:55:20 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,35 +2,6 @@
 #include <assert.h>
 #include "test.h"
 
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
-const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
-int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
-typedef struct 
-{
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
-} ColorPair;
-
-ColorPair GetColorFromPairNumber(int pairNumber) 
-{
-    ColorPair colorPair;
-    int zeroBasedPairNumber = pairNumber - 1;
-    colorPair.majorColor =(enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-    colorPair.minorColor =(enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
-    return colorPair;
-}
-
-int GetPairNumberFromColor(const ColorPair* colorPair) 
-{
-    return colorPair->majorColor * numberOfMinorColors + colorPair->minorColor + 1;
-}
-
 int main() 
 {
     testNumberToPair(4, WHITE, BROWN);
