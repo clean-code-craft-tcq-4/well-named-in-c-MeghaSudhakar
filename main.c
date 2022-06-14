729d2cf62b6e47bf8383d57783cabce9e7532229
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-06-14 15:46:30 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,11 +4,18 @@
 
 int main() 
 {
+    int i;
+    
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
 
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
 
+    
+  for(i=0; i<=25; i++)
+  {
+    printf("major color and minor color %s",testNumberToPairONLY(i));
+  }
     return 0;
 }
