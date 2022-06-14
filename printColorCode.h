9dfc298bedf0ff89d8326e39707e8e3a27981e3c
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-06-14 15:46:26 +0530

    Update printColorCode.h

--- printColorCode.h
+++ printColorCode.h
@@ -7,16 +7,7 @@ void testNumberToPairONLY(int pairNumber)
     char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
     ColorPairToString(&colorPair, colorPairNames);
     printf("Got pair %s\n", colorPairNames);
-    assert(colorPair.majorColor == expectedMajor);
-    assert(colorPair.minorColor == expectedMinor);
+
 }
 
-Void PrintColorCode()
-{
-  int i;
-  for(i=0; i<=25; i++)
-  {
-    printf("major color and minor color %s",testNumberToPairONLY(i));
-  }
-  
-}
+
