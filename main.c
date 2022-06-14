b30db4de300c34aabbad934ff8cfd1ed0cc0cf7a
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-06-14 13:29:09 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
-#include <test.c>
+#include <test.h>
 
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
